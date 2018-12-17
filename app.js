58b1678bfb546b92fee8332b0e2c18c0df5332d5
francesco <francesco.gava@studenti.unitn.it>
2018-12-17 15:35:35 +0100

    rebase completo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express'),
 
 const app = express();
 app.use(bodyParser.json());
-
+const check = require('./checker.js');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -14,6 +14,27 @@ app.set('port', (process.env.PORT || 5000));
 // a useless function that returns a fixed object. you can use it, if you want, for testing purposes
 app.get('/count',function (req, res) {
     res.json({count: 3})
+})
+
+
+app.get('/counts',function (req, res) {
+  var lato1 =parseInt(req.query.lato1)
+  var lato2=parseInt(req.query.lato2)
+  console.log(lato1,lato2)
+  res.status(200).json({'area': lato1*lato2});
+
+})
+
+app.post('/', (req, res) => {
+        var propertiesPost = req.body;
+        var num =parseInt( propertiesPost.expectedResultStatus)
+        console.log(propertiesPost.url, propertiesPost.invocationParameters,propertiesPost.expectedResultData, parseInt(propertiesPost.expectedResultStatus))
+        var response=check(propertiesPost.url, propertiesPost.invocationParameters,propertiesPost.expectedResultData, num)
+
+        .then(function(result) {
+        res.send(result)
+    });
+
 })
 
 app.listen(app.get('port'), function() {
--- checker.js
+++ checker.js
@@ -1,7 +1,8 @@
 
 const fetch = require('node-fetch')
 
-function check(url, invocationParameters,  expectedResultData, expectedResultStatus) {
+
+async  function check(url, invocationParameters,  expectedResultData, expectedResultStatus) {
 
     const checkResult = { // this is the object you need to set and return
         urlChecked: url,
@@ -13,6 +14,15 @@ function check(url, invocationParameters,  expectedResultData, expectedResultSta
 
 
 
+        const check_response = await fetch('http://localhost:5000/counts'+'?lato1='+invocationParameters.lato1+'&lato2='+invocationParameters.lato2 ,{
+                        headers: {
+                            'Accept' : 'application/json',
+                        }
+                    })
+    console.log(Object.keys(invocationParameters))
+                    const chec_json = await check_response.json()
+                    console.log(chec_json)
+          return chec_json
 }
 
 
